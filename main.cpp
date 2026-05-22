9b46038539ca6e8c95a3b202f6038211c3442b13
Broski <109301662+BitSiphon@users.noreply.github.com>
2026-05-22 16:28:24 -0400

    create dedicated connect_tcp function

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>
 #include <cerrno>
 #include <cstdlib>
 #include <cstring>
@@ -5,9 +6,75 @@
 #include <netdb.h>
 #include <string>
 #include <sys/socket.h>
-#include <sys/types.h>
+#include <unistd.h>
 
 const std::string DEFAULT_PORT = "8080";
+const int MAX_DATA_SIZE = 100; // bytes
+
+// get sockaddr, IPv4 or IPv6
+void *get_in_addr(struct sockaddr *sa) {
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in *)sa)->sin_addr);
+  }
+
+  return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+
+// Returns established socket file descriptor or -1 on error.
+int connect_tcp(std::string addr_string, std::string addr_port) {
+  // Parse addr
+
+  int sockfd;
+  int result;
+  addrinfo hints;
+  addrinfo *resolvedinfo, *p;
+  char parsed_str[INET6_ADDRSTRLEN];
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  // empty address use loopback (caused by nullptr)
+  const char *addr = (addr_string.empty() ? nullptr : addr_string.c_str());
+
+  result = getaddrinfo(addr, addr_port.c_str(), &hints, &resolvedinfo);
+  if (result != 0) {
+    std::cout << "Failed to parse address: " << gai_strerror(result) << "\n";
+    return -1;
+  };
+
+  // loop through all the results and connect to the first we can
+  for (p = resolvedinfo; p != nullptr; p = p->ai_next) {
+    sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    if (sockfd == -1) {
+      continue;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr(p->ai_addr), parsed_str,
+              sizeof parsed_str);
+    std::cout << "Attempting to connect: " << parsed_str << "\n";
+
+    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      continue;
+    }
+
+    break;
+  }
+
+  if (p == nullptr) {
+    return -1;
+  }
+
+  // Cleanup
+  freeaddrinfo(resolvedinfo);
+
+  inet_ntop(p->ai_family, get_in_addr(p->ai_addr), parsed_str,
+            sizeof parsed_str);
+  std::cout << "Established connection to " << parsed_str << "\n";
+
+  return sockfd;
+}
 
 int main(int argc, char *argv[]) {
   // Parse Args
@@ -39,32 +106,11 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  // Parse addr
-  int status;
-  addrinfo hints;
-  addrinfo *resolvedinfo;
-
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-
-  const char *addr = (addr_string.empty() ? nullptr : addr_string.c_str());
-
-  status = getaddrinfo(addr, addr_port.c_str(), &hints, &resolvedinfo);
-  if (status != 0) {
-    std::cout << "Failed to parse address: " << gai_strerror(status) << "\n";
-    return EXIT_FAILURE;
-  };
-
-  int sockfd = socket(resolvedinfo->ai_family, resolvedinfo->ai_socktype,
-                      resolvedinfo->ai_protocol);
-  if (sockfd < 0) {
-    std::cerr << "Failed to create socket: " << std::strerror(errno) << "\n";
-    freeaddrinfo(resolvedinfo);
-    return EXIT_FAILURE;
+  // Establish connection
+  int sockfd = connect_tcp(addr_string, addr_port);
+  if (sockfd == -1) {
+    std::cout << "Failed to connect to " << addr_string << ":" << addr_port
+              << "\n"
+              << std::strerror(errno) << "\n";
   }
-
-  // Cleanup
-
-  freeaddrinfo(resolvedinfo);
 }
