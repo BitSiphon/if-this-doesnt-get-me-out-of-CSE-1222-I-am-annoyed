6675b0370183f898a20897fccf430bd72f4681aa
Broski <109301662+BitSiphon@users.noreply.github.com>
2026-05-21 22:22:02 -0400

    cleanup exits, arg parse loop, and add error handling to socket creation

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cerrno>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
@@ -6,33 +7,36 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-int main(int argc, char *argv[]) {
+const std::string DEFAULT_PORT = "8080";
 
+int main(int argc, char *argv[]) {
   // Parse Args
   std::string addr_string;
-  for (int i = 0; i < argc; i++) {
+  std::string addr_port = DEFAULT_PORT;
+  for (int i = 1; i < argc; i++) {
 
     std::string argument = argv[i];
     if (argument == "-h") {
-      std::cout << "HELP\n";
-      continue;
+      std::cout << "Usage: " << argv[0] << " [-c address] [-p port]\n";
+      return EXIT_SUCCESS;
     }
     if (argument == "-c") {
       if (i + 1 < argc) {
         addr_string = argv[i + 1];
         i++;
-        continue;
       } else {
-        std::cout << "Argument '-c' called, but no address specified";
-        exit(-1);
+        std::cout << "Argument '-c' called, but no address specified\n";
+        return EXIT_FAILURE;
+      }
+    } else if (argument == "-p") {
+      if (i + 1 < argc) {
+        addr_port = argv[i + 1];
+        i++;
+      } else {
+        std::cout << "Argument '-p' called, but no port specified\n";
+        return EXIT_FAILURE;
       }
     }
-  }
-
-  // Check defaults
-  if (addr_string == "") {
-    std::cout << "No address specified";
-    exit(-1);
   }
 
   // Parse addr
@@ -44,14 +48,21 @@ int main(int argc, char *argv[]) {
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
 
-  status = getaddrinfo(addr_string.c_str(), "443", &hints, &resolvedinfo);
+  const char *addr = (addr_string.empty() ? nullptr : addr_string.c_str());
+
+  status = getaddrinfo(addr, addr_port.c_str(), &hints, &resolvedinfo);
   if (status != 0) {
-    std::cout << "Failed to parse address.\n\t" << gai_strerror(status) << "\n";
-    exit(-1);
+    std::cout << "Failed to parse address: " << gai_strerror(status) << "\n";
+    return EXIT_FAILURE;
   };
 
   int sockfd = socket(resolvedinfo->ai_family, resolvedinfo->ai_socktype,
                       resolvedinfo->ai_protocol);
+  if (sockfd < 0) {
+    std::cerr << "Failed to create socket: " << std::strerror(errno) << "\n";
+    freeaddrinfo(resolvedinfo);
+    return EXIT_FAILURE;
+  }
 
   // Cleanup
 
